ac7253f8b51dab5323a38324e3a73b4c9838b1f7
shimo85 <23301634@qq.com>
2019-01-20 10:47:11 +0800

    review features

---
+++ conf.py
@@ -0,0 +1,4 @@
+ORIGIN_ATTRIS = ['attri_1', 'attri_2', 'attri_3', 'attri_4', 'attri_5']
+
+ORIGIN_COLUMN = list(ORIGIN_ATTRIS)
+ORIGIN_COLUMN.append('value')
--- preprocess.py
+++ preprocess.py
@@ -2,26 +2,75 @@ import os
 import os.path as pth
 import pandas as pd
 import my_util as utl
+from conf import *
+
+print 'attributes: {}'.format(str(ORIGIN_ATTRIS))
+print 'columns: {}'.format(str(ORIGIN_COLUMN))
 
 
-def transfer_features(origin_data):
-    print 'start transfer features'
+def check_features(origin_data):
+    print 'check features'
+    '''
+    output csv:
+
+    timestamp, count, attri_1_items, attri_1_item_count, ...
+    
+    '''
+    out_data = {'timestamp': [], 'count': []}
+
+    for i in ORIGIN_ATTRIS:
+        out_data['{}_items'.format(i)] = []
+        out_data['{}_item_count'.format(i)] = []
+
     for timestamp_f in os.listdir(origin_data):
         ts = utl.transfer_file_name_to_timestamp(timestamp_f)
-        print ts
-        df = pd.read_csv(pth.join(origin_data, timestamp_f), encoding='utf-8', header=None,
-                         names=['attri_1', 'attri_2', 'attri_3', 'attri_4', 'attri_5', 'value'])
-        print df.head()
-        # print df.describe()
-        # print df.dtypes
+        # print ts
+        out_data['timestamp'].append(ts)
+        df = pd.read_csv(pth.join(origin_data, timestamp_f), encoding='utf-8', header=None, names=ORIGIN_COLUMN)
 
-        print df.groupby(df['attri_1']).sum()
+        out_data['count'].append(df['value'].count())
 
-        break
+        for i in ORIGIN_ATTRIS:
+            attri_temp = df[i].drop_duplicates()
+            out_data['{}_items'.format(i)].append('#'.join(attri_temp))
+            out_data['{}_item_count'.format(i)].append(attri_temp.count())
 
-    out_f_pth = pth.join('rundata', 'features_output')
+            # break
 
-    return out_f_pth
+    out_f_pth = pth.join('rundata', 'check_view.csv')
+    # print out_data
+    # print out_data.keys()
+    pd.DataFrame(out_data).sort_values(by='timestamp').to_csv(out_f_pth, index=None)
+    # return out_f_pth
+
+
+def draw_check_view():
+    f_pth = pth.join('rundata', 'check_view.csv')
+    df = pd.read_csv(f_pth)
+    import matplotlib.pyplot as plt
+    plt.figure()
+    for i in ORIGIN_ATTRIS:
+        plt.plot(df['timestamp'], df['{}_item_count'.format(i)], label=i)
+    plt.legend()
+    plt.show()
+    pass
+
+
+def desc_check_view():
+    f_pth = pth.join('rundata', 'check_view.csv')
+    df = pd.read_csv(f_pth)
+    for attri in ORIGIN_ATTRIS:
+        print '{0:-^20}'.format(attri)
+        attri_count_temp = '{}_item_count'.format(attri)
+        print 'max: {}'.format(df[attri_count_temp].max())
+        print 'min: {}'.format(df[attri_count_temp].min())
+        print 'mean: {}'.format(df[attri_count_temp].mean())
+        item_set = set()
+        for items in df['{}_items'.format(attri)]:
+            for item in items.split('#'):
+                item_set.add(item)
+        print 'item count: {}'.format(len(item_set))
+    pass
 
 
 def col_total_values(origin_data):
@@ -30,8 +79,7 @@ def col_total_values(origin_data):
     for timestamp_f in os.listdir(origin_data):
         ts = utl.transfer_file_name_to_timestamp(timestamp_f)
         data_map['timestamp'].append(ts)
-        df = pd.read_csv(pth.join(origin_data, timestamp_f), encoding='utf-8', header=None,
-                         names=['attri_1', 'attri_2', 'attri_3', 'attri_4', 'attri_5', 'value'])
+        df = pd.read_csv(pth.join(origin_data, timestamp_f), encoding='utf-8', header=None, names=ORIGIN_COLUMN)
         data_map['t_value'].append(df['value'].sum())
     t_df = pd.DataFrame(data=data_map).sort_values(by='timestamp')
     # print t_df.head()
@@ -44,6 +92,9 @@ if __name__ == '__main__':
     print 'read origin data from ', origin_f_path
     # features_out = transfer_features(origin_f_path)
     # print 'transfer features to ', features_out
-    col_total_values(origin_f_path)
+    # col_total_values(origin_f_path)
+    # check_features(origin_f_path)
+    # draw_check_view()
+    desc_check_view()
 
     pass
