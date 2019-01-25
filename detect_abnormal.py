9cba9d8e16e16d3ee561eb3742de4b96be7856c2
shimo85 <23301634@qq.com>
2019-01-26 00:01:47 +0800

    prediction l1 value and calculate dev

--- detect_abnormal.py
+++ detect_abnormal.py
@@ -1,20 +1,42 @@
 import os.path as pth
 import pandas as pd
 import numpy as np
+from conf import *
 
-if __name__ == '__main__':
+
+def cal_dev_rate(pre, val):
+    return np.float32(np.abs(pre - val) / np.float32(pre))
+
+
+def detect_t_value(max_dev_rate=.15):
     in_file = pth.join('rundata', 't_value_output', 't_values_with_pre.csv')
     df = pd.read_csv(in_file, encoding='utf-8')
-    df['dev_r'] = df.apply(lambda i: np.float32(np.abs(i['t_value'] - i['prediction']) / np.float32(i['prediction'])),
-                           axis=1)
-    max_dev_rate = .15
+    df['dev_r'] = df.apply(lambda i: cal_dev_rate(i['prediction'], i['t_value']), axis=1)
     df['label'] = df.apply(lambda i: 1 if i['dev_r'] > max_dev_rate else 0, axis=1)
     # print df.head(100)
     print 'count: {}'.format(df['label'].count())
     print 'sum: {}'.format(df['label'].sum())
-
     df.to_csv(pth.join('rundata', 't_value_output', 't_values_with_pre_dev_lable.csv'), index=0)
-
     df.loc[df['label'] == 1, ['timestamp']].to_csv(pth.join('rundata', 'abnormal_timestamp.csv'), index=None)
-
     pass
+
+
+def detect_l1_value():
+    f_path = pth.join('rundata', 'l1_value_output')
+
+    model_df = pd.read_csv(pth.join('rundata', 'check_view_model.csv'), index_col='attri')
+
+    for attri in ORIGIN_ATTRIS:
+        df = pd.read_csv(pth.join(f_path, '{}_values_with_pre.csv'.format(attri)), index_col='timestamp')
+        for item in model_df.loc[attri]['item_set'].split('#'):
+            pre_label = '{}_prediction'.format(item)
+            value_label = item
+            item_df = df[[pre_label, value_label]].dropna()
+            df['{}_dev_r'.format(item)] = item_df.apply(lambda i: cal_dev_rate(i[pre_label], i[value_label]), axis=1)
+        df.to_csv(pth.join(f_path, '{}_values_with_pre_dev.csv'.format(attri)))
+    pass
+
+
+if __name__ == '__main__':
+    # detect_t_value()
+    detect_l1_value()
--- prediction.py
+++ prediction.py
@@ -2,6 +2,7 @@ import os.path as pth
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
+from conf import *
 
 
 def prediction_by_different_classifier(df):
@@ -27,17 +28,37 @@ def append_prediction(df, pre):
     pass
 
 
-if __name__ == '__main__':
+def prediction_l1_values():
+    f_path = pth.join('rundata', 'l1_value_output')
+    for attri in ORIGIN_ATTRIS:
+        df = pd.read_csv(pth.join(f_path, '{}_values.csv'.format(attri)), index_col='timestamp')
+        for item in df.columns:
+            clf = RandomForestClassifier(max_depth=15, n_estimators=10, max_features=1)
+            item_df = df[item].dropna()
+            X_train = item_df.index.values.reshape(-1, 1)
+            y_train = item_df.values
+            try:
+                clf.fit(X_train, y_train)
+                predictions = clf.predict(X_train)
+                df['{}_prediction'.format(item)] = pd.Series(predictions, index=item_df.index)
+            except Exception, ex:
+                print 'Error: %s' % ex.message
+        df.to_csv(pth.join(f_path, '{}_values_with_pre.csv'.format(attri)))
+    pass
+
+
+def prediction_t_values():
     f_path = pth.join('rundata', 't_value_output', 't_values.csv')
     df = pd.read_csv(f_path, encoding='utf-8')
-
     pre_values = prediction_by_different_classifier(df)
-
     # plt.figure()
     # plt.plot(df['timestamp'], df['t_value'])
     # plt.plot(df['timestamp'], pre_values, label='prediction')
     # plt.legend()
     # plt.show()
-
     append_prediction(df, pre_values)
 
+
+if __name__ == '__main__':
+    # prediction_t_values()
+    prediction_l1_values()
