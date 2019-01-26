969d2173dbbdd81aa5f07e73be0df43edd2e2834
shimo85 <23301634@qq.com>
2019-01-26 15:05:03 +0800

    try cluster abnormal timestamp

--- prediction.py
+++ prediction.py
@@ -1,7 +1,7 @@
 import os.path as pth
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.cluster import KMeans
 from conf import *
 
 
@@ -51,14 +51,23 @@ def prediction_t_values():
     f_path = pth.join('rundata', 't_value_output', 't_values.csv')
     df = pd.read_csv(f_path, encoding='utf-8')
     pre_values = prediction_by_different_classifier(df)
-    # plt.figure()
-    # plt.plot(df['timestamp'], df['t_value'])
-    # plt.plot(df['timestamp'], pre_values, label='prediction')
-    # plt.legend()
-    # plt.show()
     append_prediction(df, pre_values)
+
+
+def cluster_abnormal_ts():
+    df = pd.read_csv(pth.join('rundata', 'abnormal_timestamp.csv'))
+    K = range(2, 10)
+    for k in K:
+        clf = KMeans(n_clusters=k)
+        print 'n_clusters: {}'.format(k)
+        clf_pre = clf.fit_predict(df['timestamp'].reshape(-1, 1))
+        print clf_pre
+        # break
+    pass
 
 
 if __name__ == '__main__':
     # prediction_t_values()
-    prediction_l1_values()
+    # prediction_l1_values()
+    cluster_abnormal_ts()
+    pass
